2e30ebadee4969128f6d3bbca9f97287d02d0b81
ajaynegi45 <ajaynegi3345@icloud.com>
2024-01-23 22:27:30 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ const Language = lazy(()=> import("./Pages/Language/Language.jsx"));
 const Kumaoni = lazy(()=> import("./Pages/Language/Kumaoni.jsx"));
 const Garhwali = lazy(()=> import("./Pages/Language/Garhwali.jsx"));
 const ErrorPage = lazy(()=> import("./components/ErrorPage.jsx"));
-const App = lazy(()=> import('./App.jsx'));
-const Layout = lazy(()=> import("./Layout.jsx"));
+// const App = lazy(()=> import('./App.jsx'));
+// const Layout = lazy(()=> import("./Layout.jsx"));
 // Import Pages
-// import App from './App.jsx'
+import App from './App.jsx'
 // import ErrorPage from "./components/ErrorPage.jsx";
-// import Layout from "./Layout.jsx";
+import Layout from "./Layout.jsx";
 // import Language from "./Pages/Language/Language.jsx";
 // import Kumaoni from "./Pages/Language/Kumaoni.jsx";
 // import Garhwali from "./Pages/Language/Garhwali.jsx";
@@ -19,8 +19,8 @@ import './index.css'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path='/' element={ <Suspense> <Layout/> </Suspense>} >
-            <Route path='' element={<Suspense> <App/> </Suspense>} />
+        <Route path='/' element={ <Layout/> } >
+            <Route path='' element={ <App/> } />
             <Route path='language' element={ <Suspense> <Language/> </Suspense>} />
             <Route path='language/kumaoni' element={<Suspense> <Kumaoni/> </Suspense>} />
             <Route path='language/garhwali' element={<Suspense> <Garhwali/> </Suspense>} />

873ac318e6193c2d36c8dafec11079f3aaa592b4
Ajay Negi <ajaynegi3345@icloud.com>
2024-02-22 23:53:50 +0530

    Update

--- src/Pages/Community/Community.jsx
+++ src/Pages/Community/Community.jsx
@@ -4,17 +4,31 @@ import "../Community/community.css";
 
 function Community() {
     return (
-        <div className={"community-container"} >
+        <div className={"community-container"}>
             <div className={"community-header"}>
-                <img src="https://i0.wp.com/ukdigital.in/wp-content/uploads/2021/07/ukdigital-community-cover.jpg?fit=1300%2C333&ssl=1" alt="" />
+                <img
+                    src="https://i0.wp.com/ukdigital.in/wp-content/uploads/2021/07/ukdigital-community-cover.jpg?fit=1300%2C333&ssl=1"
+                    alt=""/>
             </div>
 
             <div>
 
             </div>
 
-            <div>
-                <Card heading={""} description={""} link={""} img={""} imgDescription={""} />
+            <h1 className="explore-uttarakhand-title">Explore Uttarakhand Community</h1>
+
+            <div className={"community-list"}>
+                <Card heading={""} description={""} link={""} img={""} imgDescription={""}/>
+
+                <Card heading={""} description={""} link={""} img={""} imgDescription={""}/>
+
+                <Card heading={""} description={""} link={""} img={""} imgDescription={""}/>
+
+                <Card heading={""} description={""} link={""} img={""} imgDescription={""}/>
+
+                <Card heading={""} description={""} link={""} img={""} imgDescription={""}/>
+
+                <Card heading={""} description={""} link={""} img={""} imgDescription={""}/>
 
             </div>
 

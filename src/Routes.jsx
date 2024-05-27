dd4c10c4956e8b5756c29c137293693b53c30550
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-27 23:33:47 +0530

    Update

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -40,6 +40,7 @@ const router = createBrowserRouter(
     createRoutesFromElements(
 
         <Route path='/' element={<Layout />}>
+
             <Route path='' element={<App />} />
 
             {/* Language */}
@@ -89,7 +90,7 @@ const router = createBrowserRouter(
             <Route path='community/*' element={<UnderWorkingPage />} />
 
 
-            {/* Community */}
+            {/* Utensil */}
             <Route path='utensils' element={<Utensil />} />
             <Route path='utensils/*' element={<UnderWorkingPage />} />
 
@@ -104,11 +105,14 @@ const router = createBrowserRouter(
 
             {/* Error */}
             <Route path='*' element={<ErrorPage />} />
+
         </Route>
     )
 );
 ReactDOM.createRoot(document.getElementById('root')).render(
+
         <Suspense fallback={<div className={"loading"}> <img src={"/loading.svg"}/> </div>}>
             <RouterProvider router={router} />
         </Suspense>
+
 );
